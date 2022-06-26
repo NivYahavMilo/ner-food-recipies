bbfad7ad72517003ae62ac64de85a89cfd38430c
Niv Yahav <niv.yahav@stargo.co>
2022-06-26 17:01:17 +0300

    iob tagging

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,16 +1,11 @@
 import json
 import os
 import re
-
-import pandas as pd
-import spacy
-from spacy.training import offsets_to_biluo_tags
-
 import config
-
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 ENTITIES = ["FOOD", "QUANTITY", "UNIT", "PROCESS", "PHYSICAL_QUALITY", "COLOR",
             "TASTE", "PURPOSE", "PART"]
-NLP = spacy.load("en_core_web_sm")
 
 
 def _read_csv(dataset):
@@ -18,74 +13,6 @@ def _read_csv(dataset):
         os.path.join(config.DATA_PATH, f'{dataset}.csv')
     )
     return data
-
-
-def _span_to_biluo(recipe, span_entities):
-    """
-    :param span_entities: list of span entities, eg. [(span_start, span_end,
-    "FOOD"), (span_start, span_end, "PROCESS")]
-    :return: list of BILUO entities, eg. ["O", "B-FOOD", "L-FOOD",
-    "U-PROCESS"] along with tokenized recipe
-    """
-    doc = NLP(recipe.replace("\n", " "))
-    tokenized_recipe = [token.text for token in doc]
-    spans = offsets_to_biluo_tags(doc, span_entities)
-    return tokenized_recipe, spans
-
-
-def _biluo_to_iob(biluo_entities):
-    """
-    :param biluo_entities: list of BILUO entities, eg. ["O", "B-FOOD", "L-FOOD",
-    "U-PROCESS"]
-    :return: list of BIO entities, eg. ["O", "B-FOOD", "I-FOOD", "B-PROCESS"]
-    """
-    bio_entities = [entity.replace("L-", "I-").replace("U-", "B-")
-                    for entity in biluo_entities]
-    return bio_entities
-
-
-def _span_to_iob(recipe, span_entities):
-    """
-    :param span_entities: list of span entities, eg. [(span_start, span_end,
-    "FOOD"), (span_start, span_end, "PROCESS")]
-    :return: list of BIO entities, eg. ["O", "B-FOOD", "I-FOOD", "B-PROCESS"]
-    """
-    tokenized_recipe, biluo_entities = _span_to_biluo(recipe, span_entities)
-    bio_entities = _biluo_to_iob(biluo_entities)
-    return tokenized_recipe, bio_entities
-
-
-def prepare_data(df, entities_format="spans"):
-    """
-    df: TASTEset as pd.DataFrame or a path to the TASTEset
-    entities_format: the format of entities. If equal to 'bio', entities
-    will be of the following format: [[B-FOOD, I-FOOD, O, ...], [B-UNIT, ...]].
-    If equal to span, entities will be of the following format:
-    [[(0, 6, FOOD), (10, 15, PROCESS), ...], [(0, 2, UNIT), ...]]
-    return:
-    list of recipes and corresponding list of entities
-    """
-
-    all_recipes = df["ingredients"].to_list()
-    all_entities = []
-
-    for idx in df.index:
-        ingredients_entities = json.loads(df.at[idx, "ingredients_entities"])
-        entities = []
-
-        for entity_dict in ingredients_entities:
-            entities.append((entity_dict["start"], entity_dict["end"],
-                             entity_dict["type"]))
-
-        if entities_format == "IOB":
-            tokenized_recipe, entities = _span_to_iob(all_recipes[idx],
-                                                      entities)
-            all_recipes[idx] = tokenized_recipe
-
-        all_entities.append(entities)
-
-    return all_recipes, all_entities
-
 
 def preprocess_data():
     data = _read_csv("TASTEset")
@@ -142,8 +69,24 @@ def _data_preprocessing():
         post_df['sample'][jj:jj+len(entities)] = ii+1
         jj += len(entities)
 
+        post_df = form_iob_tagging_format(post_df)
+
     post_df = post_df.reset_index(drop=True)
     pass
+
+def form_iob_tagging_format(df_section: pd.DataFrame):
+    for idx, row in df_section.iterrows():
+        if len(row['entity'].split())==1:
+            df_section.loc[idx, 'tag'] = f'B-{row["type"]}'
+        else:
+            df_section['entity'][idx] = row['entity'].split()
+            df_section = df_section.explode(['entity'])
+            pass
+
+    return df_section
+
+
+
 
 
 if __name__ == '__main__':
