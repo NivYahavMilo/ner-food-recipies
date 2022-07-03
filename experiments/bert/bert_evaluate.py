ecdad81f4c986c407a05f3fbf0c41014d1a4d3ad
Niv Yahav <niv.yahav@stargo.co>
2022-07-03 18:01:05 +0300

    added evaluations metrics for bert model

--- experiments/bert/bert_evaluate.py
+++ experiments/bert/bert_evaluate.py
@@ -3,28 +3,34 @@ import os
 from flair.data import Corpus
 from flair.datasets import ColumnCorpus, DataLoader
 from flair.models import SequenceTagger
+from flair.data import Sentence
 from flair.visual.training_curves import Plotter
 
 import config
+
+
 
 
 def evaluate():
 
     columns = {0: 'text', 1: 'ner'}
     # init a corpus using column format data folder and the names of the train, dev and test files
-    corpus: Corpus = ColumnCorpus(config.DATA_PATH, columns,
+    corpus: Corpus = ColumnCorpus(config.DATA_PATH,
+                                  column_format=columns,
                                   train_file='train.txt',
                                   test_file='test.txt',
                                   dev_file='dev.txt')
 
-    eval_data = DataLoader(corpus.test, batch_size=1)
-
-
+    corpus.make_label_dictionary('ner')
     ner_model = SequenceTagger.load(os.path.join(config.BERT_PATH,
                                                        "final-model.pt"))
 
-    results, score = ner_model.evaluate(eval_data,
-                                        out_path=fr"{config.BERT_PATH}\\predictions.txt")
+    result = ner_model.evaluate(corpus.test,
+                                   gold_label_type='ner',
+                                   out_path=fr"{config.BERT_PATH}\\predictions.txt"
+                                   )
+
+    print(result.detailed_results)
 
     # visualize
     plotter = Plotter()
